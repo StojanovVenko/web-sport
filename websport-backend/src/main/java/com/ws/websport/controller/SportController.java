468752a1ed49cbfb37281596be4289788e9d3759
ristovkiril <ristov.kiril@yahoo.com>
2021-06-30 16:30:26 +0200

    Capitalize words in search

--- websport-backend/src/main/java/com/ws/websport/controller/SportController.java
+++ websport-backend/src/main/java/com/ws/websport/controller/SportController.java
@@ -2,6 +2,8 @@ package com.ws.websport.controller;
 
 import com.ws.websport.model.Sport;
 import com.ws.websport.service.SportService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +19,6 @@ public class SportController {
 
     @GetMapping("/search")
     public Sport getSportInfo(@RequestParam("sport") String sportName) {
-        return this.sportService.getSportInfo(sportName);
+        return this.sportService.getSportInfo(WordUtils.capitalizeFully(sportName));
     }
 }
